89e54e74106814ecf6d11d98208c85eb3a4fd678
Kim Fisher <kfisher@wcs.org>
2021-12-02 12:19:04 -0500

    output unconditional_prob

--- src/probability/probability_panthera_tigris.py
+++ src/probability/probability_panthera_tigris.py
@@ -234,6 +234,8 @@ def jags_post_process(jags_output, df_poly_detections):
     for i in range(len(jags_output["phi0"])):
         phi0_mean.append(jags_output["phi0"][i].flatten().mean())
 
+    phi0_mean_round = [round(100 * i) for i in phi0_mean]
+
     effort = [0] * len(jags_output[PROBABILITY])
     for i in range(len(jags_output[PROBABILITY])):
         if (
@@ -251,6 +253,7 @@ def jags_post_process(jags_output, df_poly_detections):
             "only_ah": df_poly_detections["only_ah"],
             "surveyed": df_poly_detections["surveyed"],
             PROBABILITY: phi_mean_round,
+            "unconditional_prob": phi0_mean_round,
             EFFORT: effort,
         }
     )
